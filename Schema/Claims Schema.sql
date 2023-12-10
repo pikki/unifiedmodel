45201e037965c4752858b8f5df94cf2946f53b63
pikki <himalay.kilaru@gmail.com>
2023-12-10 16:42:37 -0500

    changed claims and empi to snowflake

--- Schema/Claims Schema.sql
+++ Schema/Claims Schema.sql
@@ -1,6 +1,4 @@
-
-
-CREATE TABLE Eligibility (
+CREATE OR REPLACE TABLE Eligibility (
     patient_id STRING,
     member_id STRING,
     birth_date DATE,
@@ -14,13 +12,12 @@ CREATE TABLE Eligibility (
     dual_status_code STRING,
     medicare_status_code STRING,
     data_source STRING,
-    UniqueRowID int default 0 NULL,
-    LoadDate datetime NULL
+    UniqueRowID int default 0,
+    LoadDate datetime
 );
 
 
--- Dimension Tables
-CREATE TABLE DimEncounter (
+CREATE OR REPLACE TABLE DimEncounter (
     encounter_id STRING PRIMARY KEY,
     admission_date DATE,
     discharge_date DATE,
@@ -34,24 +31,56 @@ CREATE TABLE DimEncounter (
     revenue_center_code STRING
 );
 
-CREATE TABLE DimServiceGroup (
+CREATE OR REPLACE TABLE DimServiceGroup (
     service_category_1 STRING,
     service_category_2 STRING,
     PRIMARY KEY (service_category_1, service_category_2)
 );
 
-CREATE TABLE DimProvider (
-    npi STRING PRIMARY KEY,
-    -- Add additional provider-related attributes as needed
+
+CREATE OR REPLACE TABLE DimProvider (
+    npi STRING PRIMARY KEY,  
+    provider_name STRING,
+    specialty STRING,
+    address STRING,
+    phone STRING,
+    email STRING,
+    provider_type STRING,
+    rendering_npi STRING,  
+    billing_npi STRING,    
+    facility_npi STRING   
 );
 
-CREATE TABLE DimDate (
-    date DATE PRIMARY KEY
-    -- Add additional date-related attributes as needed
+
+
+CREATE OR REPLACE TABLE DimDate (
+    date DATE PRIMARY KEY,
+    day_of_week STRING,
+    is_weekend BOOLEAN,
+    month INT,
+    year INT,
+    quarter INT,
+    day_of_month INT,
+    week_of_year INT
 );
 
--- Fact Table
-CREATE TABLE FactMedicalClaim (
+
+CREATE OR REPLACE TABLE DimEncounter (
+    encounter_id STRING PRIMARY KEY,
+    admission_date DATE,
+    discharge_date DATE,
+    admit_source_code STRING,
+    admit_type_code STRING,
+    discharge_disposition_code STRING,
+    place_of_service_code STRING,
+    bill_type_code STRING,
+    ms_drg_code STRING,
+    apr_drg_code STRING,
+    revenue_center_code STRING
+);
+
+/*
+CREATE OR REPLACE TABLE FactMedicalClaim (
     claim_id STRING PRIMARY KEY,
     claim_line_number INT,
     encounter_id STRING REFERENCES DimEncounter(encounter_id),
@@ -85,23 +114,52 @@ CREATE TABLE FactMedicalClaim (
     deductible_amount NUMERIC,
     total_cost_amount NUMERIC,
     data_source STRING,
-    UniqueRowID int default 0 NULL,
-    LoadDate datetime NULL
+    UniqueRowID int default 0,
+    LoadDate datetime
 );
 
--- Dimension Tables
-CREATE TABLE DimProvider (
-    npi STRING PRIMARY KEY,
-    -- Add additional provider-related attributes as needed
+*/
+
+CREATE OR REPLACE TABLE FactMedicalClaim (
+    claim_id STRING PRIMARY KEY,
+    claim_line_number INT,
+    encounter_id STRING,
+    claim_type STRING,
+    patient_id STRING,
+    member_id STRING,
+    payer STRING,
+    plan STRING,
+    claim_start_date DATE,
+    claim_end_date DATE,
+    claim_line_start_date DATE,
+    claim_line_end_date DATE,
+    service_category_1 STRING,
+    service_category_2 STRING,
+    admit_source_code STRING,
+    admit_type_code STRING,
+    discharge_disposition_code STRING,
+    place_of_service_code STRING,
+    bill_type_code STRING,
+    ms_drg_code STRING,
+    apr_drg_code STRING,
+    rendering_npi STRING,
+    billing_npi STRING,
+    facility_npi STRING,
+    paid_date DATE,
+    paid_amount NUMERIC,
+    allowed_amount NUMERIC,
+    charge_amount NUMERIC,
+    coinsurance_amount NUMERIC,
+    copayment_amount NUMERIC,
+    deductible_amount NUMERIC,
+    total_cost_amount NUMERIC,
+    data_source STRING,
+    UniqueRowID int default 0,
+    LoadDate datetime
 );
 
-CREATE TABLE DimDate (
-    date DATE PRIMARY KEY
-    -- Add additional date-related attributes as needed
-);
 
--- Fact Table
-CREATE TABLE FactPrescriptionClaim (
+CREATE OR REPLACE TABLE FactPrescriptionClaim (
     claim_id STRING PRIMARY KEY,
     claim_line_number INTEGER,
     patient_id STRING,
@@ -122,6 +180,7 @@ CREATE TABLE FactPrescriptionClaim (
     copayment_amount NUMERIC,
     deductible_amount NUMERIC,
     data_source STRING,
-    UniqueRowID int default 0 NULL,
-    LoadDate datetime NULL
+    UniqueRowID int default 0,
+    LoadDate datetime
 );
+
